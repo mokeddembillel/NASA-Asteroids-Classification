dead20c0a19c07091273c3b6508b9b7672919f87
billel mokeddem <bilel.crysis3@gmail.com>
2020-12-02 23:30:32 +0100

    Checking Missing values

--- NASA-Asteroids-Classification.py
+++ NASA-Asteroids-Classification.py
@@ -4,8 +4,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Reading the Data
-dataset = pd.read_csv('./Data/nasa.csv', sep=',')
+dataset = pd.read_csv('./Data/nasa.csv', sep=',', na_values=["n/a", "na", "--"])
 
 X = dataset.iloc[:, :-1]
 y = dataset.iloc[:, -1]
+
+# Cheking Missing Values
+dataset.isnull().any()
+
+
 
